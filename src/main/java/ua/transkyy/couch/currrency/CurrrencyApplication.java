60867e1f999a14f8103593e0434bb53b6c12cf38
Andriy <transkyy@gmail.com>
2023-10-10 15:20:05 +0300

    Initial commit

---
+++ src/main/java/ua/transkyy/couch/currrency/CurrrencyApplication.java
@@ -0,0 +1,13 @@
+package ua.transkyy.couch.currrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/transkyy/couch/currrency/CurrrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.transkyy.couch.currrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
